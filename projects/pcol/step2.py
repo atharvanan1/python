6b1035287474ad547afaf921d7592dde79a86803
Atharva Nandanwar <Azzentys>
2019-07-03 03:09:20 +0530

    Added a project - pcol

--- projects/pcol/step2.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-import argparse
-
-parser = argparse.ArgumentParser(prog='pcol')
-parser.add_argument('--foo', nargs='?', help='foo of the %(prog)s program')
-parser.add_argument('bar', nargs='+', help='bar of the %(prog)s program')
-args = parser.parse_args()
-print(args)
